86c016bafa8f41889bd21dcf63d3d93a6fbdf368
Scott McCrindle <iainmccrindle@gmail.com>
2025-03-20 22:19:57 -0400

    Added an alternate JS using the toggle event on the details element

---
+++ lesson-10/js/accordion2.js
@@ -0,0 +1,18 @@
+// STEP 6: Nab all the SUMMARY elements
+
+
+// STEP 7: Loop through the element array, and add an event listener for the toggle event
+
+	
+		// STEP 8a: Check to see whether the DETAILS event that was toggled has the "open" attribute
+		
+			// STEP 8b: The browser just added the "open" attribute, so this DETAILS element was previously closed
+			
+				// STEP 8c: If this DETAILS element is NOT the one that was just clicked, close it by removing the "open" attribute
+				
+			
+		
+	
+
+
+// This experiment based on https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details
