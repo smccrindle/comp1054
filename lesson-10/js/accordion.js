6906e9234d3e9427755f4a1fd580a078ce363291
Scott McCrindle <iainmccrindle@gmail.com>
2026-03-20 11:30:39 -0400

    Improved the learning progression for the accordion JS and HTML and added a step for CSS animation

--- lesson-10/js/accordion.js
+++ lesson-10/js/accordion.js
@@ -1,14 +1,19 @@
 // STEP 6: Nab all the SUMMARY elements
 
 
-// STEP 7: Loop through the element array to add an event listener, and reference a function called closeOpenDetails
+// STEP 7: Loop through the element array, and add an event listener for the toggle event
 
+	
+		// STEP 8a: Check to see whether the DETAILS event that was toggled has the "open" attribute
+		
+			// STEP 8b: The browser just added the "open" attribute, so this DETAILS element was previously closed
+			
+				// STEP 8c: If this DETAILS element is NOT the one that was just clicked, close it by removing the "open" attribute
+				
+			
+		
+	
 
-// STEP 8a: Create the closeOpenDetails() function
+// STEP 9: Verify that it works in the browser, then proceed to accordion.css for the next step
 
-	// STEP 8b: Loop through the summaries array again
-
-		// STEp 8c: Check to make sure the DETAILS element is not the parent of the SUMMARY that was clicked 
-
-
-// STEP 9: Check in the browser to make sure there are no errors - use the console, then proceed to the CSS for the final step
+// This experiment based on https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details
